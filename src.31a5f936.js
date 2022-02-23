95cb917e39c816a4ce6c87af70ad929577b7e8bb
yurilov <89077590+yurilov@users.noreply.github.com>
2022-02-23 08:25:55 +0000

    Deploying to gh-pages from @ yurilov/resume@35dc95ae151c35e2ef1c191d78e20713ab581746 🚀

--- src.31a5f936.js
+++ src.31a5f936.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/resume/src.edbb42a8.js.map
+//# sourceMappingURL=/resume/src.31a5f936.js.map
